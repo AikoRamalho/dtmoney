6c4bef04a4d99e7bc93a032cc16f468b7f29a2bd
Aiko Ramalho <aiko.ramalho@gmail.com>
2022-09-12 22:24:46 -0300

    feat: create useTransaction hook and get its amount from it

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -2,11 +2,30 @@ import { useContext } from 'react';
 import incomeImg  from '../../assets/income.svg'
 import outcomeImg  from '../../assets/outcome.svg'
 import totalImg  from '../../assets/total.svg'
-import { TransactionsContext } from '../../TransactionsContext';
+import { useTransactions } from '../../hooks/useTransactions';
+import { Transaction } from '../../protocols/transaction';
 import { Container } from "./styles";
 
+interface MappedValues {
+  [key: string]: number;
+}
+
 export function Summary() {
-  const transactions = useContext(TransactionsContext);
+  const { transactions } = useTransactions();
+
+  const mappedValues: MappedValues = {
+    deposit: 0,
+    withdraw: 0,
+    total: 0,
+  }
+
+  const summary = transactions.reduce((mappedValues: MappedValues, transaction: Transaction) => {
+    mappedValues[transaction.type] += transaction.amount;
+
+    transaction.type === 'deposit' ? mappedValues.total += transaction.amount : mappedValues.total -= transaction.amount;
+
+    return mappedValues;
+  }, mappedValues)
 
   return (
     <Container>
@@ -15,21 +34,42 @@ export function Summary() {
           <p> Income </p>
           <img src={incomeImg} alt="Income" /> 
         </header>
-        <strong> $10000 </strong>
+        <strong> 
+          {
+            new Intl.NumberFormat('en-US', {
+              style: 'currency',
+              currency: 'USD'
+            }).format(summary.deposit)
+          } 
+        </strong>
       </div>
       <div>
         <header>
           <p> Outcome </p>
           <img src={outcomeImg} alt="Outcome" /> 
         </header>
-        <strong> - $4000 </strong>
+        <strong> 
+          -{
+            new Intl.NumberFormat('en-US', {
+              style: 'currency',
+              currency: 'USD'
+            }).format(summary.withdraw)
+          } 
+        </strong>
       </div>
       <div className="highlight-background">
         <header>
           <p> Total </p>
           <img src={totalImg} alt="Total" /> 
         </header>
-        <strong> $6000 </strong>
+        <strong> 
+          {
+            new Intl.NumberFormat('en-US', {
+              style: 'currency',
+              currency: 'USD'
+            }).format(summary.total)
+          } 
+        </strong>
       </div>
     </Container>
   )
